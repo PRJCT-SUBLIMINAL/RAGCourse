18ab3cd2823e07cd9e20efa06057c4e70eaad7d7
Baggers <prjct.subliminal@gmail.com>
2026-07-14 22:56:38 +0000

    score normalization

--- rag-search-engine/cli/hybrid_search_cli.py
+++ rag-search-engine/cli/hybrid_search_cli.py
@@ -2,11 +2,32 @@ import argparse
 
 def main() -> None:
     parser = argparse.ArgumentParser(description="Hybrid Search CLI")
-    parser.add_subparsers(dest="command", help="Available commands")
+    subparsers = parser.add_subparsers(dest="command", help="Available commands")
+
+    normalize_parser = subparsers.add_parser("normalize", help="Normalize a list of scores")
+    normalize_parser.add_argument("scores_list", nargs="*", type=float, help="A list of scores, inf args")
 
     args = parser.parse_args()
 
     match args.command:
+        case "normalize":
+            if args.scores_list is None:
+                raise ValueError("No scores were given.")
+
+            min_score = min(args.scores_list)
+            max_score = max(args.scores_list)
+
+            values = []
+
+            for score in args.scores_list:
+                if min_score == max_score:
+                    values.append(1.0)
+                else:
+                    values.append((score - min_score) / (max_score - min_score))
+
+            for value in values:
+                print(f"* {value:.4f}")
+
         case _:
             parser.print_help()
 
