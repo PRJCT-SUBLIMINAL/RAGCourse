50155c37bbda323d0021ced11a4de39a57f5b117
Baggers <prjct.subliminal@gmail.com>
2026-07-15 13:50:44 +0000

    query expansion added

--- rag-search-engine/cli/hybrid_search_cli.py
+++ rag-search-engine/cli/hybrid_search_cli.py
@@ -17,7 +17,7 @@ def main() -> None:
     rrf_search_parser.add_argument("query", type=str, help="The query to run the rrf search with")
     rrf_search_parser.add_argument("-k", type=int, default=60, help="The k parameter to control how much more weight to give")
     rrf_search_parser.add_argument("--limit", type=int, default=5, help="Limit the search results to this value")
-    rrf_search_parser.add_argument("--enhance", type=str, choices=["spell", "rewrite"], help="Query enhancement method")
+    rrf_search_parser.add_argument("--enhance", type=str, choices=["spell", "rewrite", "expand"], help="Query enhancement method")
 
     args = parser.parse_args()
 
